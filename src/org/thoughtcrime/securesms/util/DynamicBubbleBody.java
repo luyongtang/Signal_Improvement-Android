5923b219ea44da4685ac99567ce8c97c2be759d6
Manpreet <many_nangla@hotmail.com>
2019-02-06 13:00:49 -0500

    #17 Changed DynamicBubbleBody; made Activity parameter to Context object

--- src/org/thoughtcrime/securesms/util/DynamicBubbleBody.java
+++ src/org/thoughtcrime/securesms/util/DynamicBubbleBody.java
@@ -1,16 +1,17 @@
 package org.thoughtcrime.securesms.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
 public class DynamicBubbleBody {
     private String currentBubbleColor;
 
-    public void onCreate(Activity activity) {
-        currentBubbleColor = getSelectedBubbleColor(activity);
+    public void onCreate(Context context) {
+        currentBubbleColor = getSelectedBubbleColor(context);
     }
 
-    public void onResume(Activity activity) {
+    /*public void onResume(Activity activity) {
         if (currentBubbleColor != getSelectedBubbleColor(activity)) {
             Intent intent = activity.getIntent();
             activity.finish();
@@ -18,16 +19,16 @@ public class DynamicBubbleBody {
             activity.startActivity(intent);
             DynamicBubbleBody.OverridePendingTransition.invoke(activity);
         }
+    }*/
+
+    public String getSelectedBubbleColor(Context context) {
+        return TextSecurePreferences.getBubble(context);
     }
 
-    public String getSelectedBubbleColor(Activity activity) {
-        return TextSecurePreferences.getBubble(activity);
-    }
-
-    private static final class OverridePendingTransition {
-        static void invoke(Activity activity) {
-            activity.overridePendingTransition(0, 0);
+    /*private static final class OverridePendingTransition {
+        static void invoke(Context context) {
+            context.overridePendingTransition(0, 0);
         }
-    }
+    }*/
 }
 
