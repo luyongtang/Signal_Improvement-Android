ffe93407fcc0651a2f5f21650fd30f12499c3a4b
Manpreet <many_nangla@hotmail.com>
2019-02-12 23:58:22 -0500

    Test Implemented for Bubble Color #33
    Created the UI test for changing the color of the body bubble color. At the same time, reduced the color options for all preference to 5 colors only

--- test/androidTest/java/org/thoughtcrime/securesms/PreferenceBubbleBodyColorTest.java
+++ test/androidTest/java/org/thoughtcrime/securesms/PreferenceBubbleBodyColorTest.java
@@ -8,6 +8,12 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class PreferenceBubbleBodyColorTest {
@@ -17,6 +23,15 @@ public class PreferenceBubbleBodyColorTest {
 
     @Test
     public void changeBodyBubbleColorTest(){
-        //Your test here
+        //Chosen color to apply on bubble body chat
+        String newColor = "Lime";
+        //Navigate to Bubble Color preference
+        onView(withText("Customization")).perform(click());
+        onView(withText("Bubble Color")).perform(click());
+        //select the color from list
+        onView(withText(newColor)).perform(click());
+        //the new selected color is now displayed as the selected one,
+        // which is the reference value used to create the color for the bubble body chat
+        onView(withText(newColor)).check(matches(isDisplayed()));
     }
 }
