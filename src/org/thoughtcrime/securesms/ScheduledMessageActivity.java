d39baa68550682da248f2c5808e5e0e7c25afddc
David Bechara <davips96@gmail.com>
2019-03-27 15:14:53 -0400

    #27 Small change for Scheduled Message tests

--- src/org/thoughtcrime/securesms/ScheduledMessageActivity.java
+++ src/org/thoughtcrime/securesms/ScheduledMessageActivity.java
@@ -61,51 +61,60 @@ public class ScheduledMessageActivity extends AppCompatActivity {
         int year = current.get(Calendar.YEAR);
         int month = current.get(Calendar.MONTH);
         int day = current.get(Calendar.DAY_OF_MONTH);
+
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, TimePickerFragment.TIME_HOURS);
         calendar.set(Calendar.MINUTE, TimePickerFragment.TIME_MINUTE);
         calendar.set(Calendar.YEAR, DatePickerFragment.DATE_YEAR);
         calendar.set(Calendar.MONTH, DatePickerFragment.DATE_MONTH);
         calendar.set(Calendar.DAY_OF_MONTH, DatePickerFragment.DATE_DAY);
+
         long delay= calendar.getTimeInMillis() - System.currentTimeMillis();
+
         messageBody = ((EditText) findViewById(R.id.scheduled_body)).getText().toString();
+
         boolean dateNull = DatePickerFragment.DATE_YEAR == 0;
         boolean timeNull = TimePickerFragment.TIME_HOURS == 0;
         boolean msgNull = messageBody.isEmpty();
 
         if (dateNull || timeNull || msgNull) {
+
             String alertMsg = "Please select the following: \n";
-            if(dateNull){
+            if(dateNull)
                 alertMsg = alertMsg.concat("Date\n");
-            }
-            if(timeNull){
+            if(timeNull)
                 alertMsg = alertMsg.concat("Time\n");
-            }
-            if(msgNull){
+            if(msgNull)
                 alertMsg = alertMsg.concat("Message\n");
-            }
+
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage(alertMsg);
             AlertDialog alert = builder.create();
             alert.show();
-        }
-        else if (delay <= 0){
+
+        } else if (delay <= 0) {
+
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Time must be in the future");
             AlertDialog alert = builder.create();
             alert.show();
-        }
-        else{
+
+        } else {
+
             Handler handler = new Handler();
+
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     OutgoingTextMessage message;
+
                     message = new OutgoingTextMessage(recipient, messageBody, -1);
                     MessageSender.send(context, message, threadId, false, null);
                 }
             }, delay);
+
             onBackPressed();
+
         }
     }
 }
---
+++ test/androidTest/java/org/thoughtcrime/securesms/ScheduledMessagesUnitTests.java
@@ -0,0 +1,30 @@
+package org.thoughtcrime.securesms;
+
+import android.content.Context;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import androidx.test.filters.LargeTest;
+
+@LargeTest
+public class ScheduledMessagesUnitTests {
+
+    @Inject
+    Context mContext;
+
+    ScheduledMessageActivity scheduledMessageActivity;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
