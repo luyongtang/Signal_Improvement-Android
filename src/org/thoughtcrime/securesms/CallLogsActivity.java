70d9fd4784ace62b7a845480147f30f028de1e17
Mike Salib <mikeez@mikesalib.com>
2019-03-29 16:36:28 -0400

    #65 - Displaying NavBar Call Log

--- src/org/thoughtcrime/securesms/CallLogsActivity.java
+++ src/org/thoughtcrime/securesms/CallLogsActivity.java
@@ -1,25 +1,36 @@
 package org.thoughtcrime.securesms;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.thoughtcrime.securesms.components.CallLogsFragment;
 import org.thoughtcrime.securesms.components.NavigationBarFragment;
 
-public class CallLogsActivity extends PassphraseRequiredActionBarActivity implements  NavigationBarFragment.OnFragmentInteractionListener{
+public class CallLogsActivity extends PassphraseRequiredActionBarActivity implements NavigationBarFragment.OnFragmentInteractionListener, CallLogsFragment.OnFragmentInteractionListener {
 
-    private NavigationBarFragment    navigationBarFragment;
+    private NavigationBarFragment navigationBarFragment;
     private ViewGroup navigationBarContainer;
+    private CallLogsFragment callLogsFragment;
+    private ViewGroup callLogsContainer;
+    private TextView callLogTV;
 
     @Override
     protected void onCreate(Bundle icicle, boolean ready) {
         setContentView(R.layout.activity_call_logs);
-        navigationBarContainer   = findViewById(R.id.call_log_navigation_bar_container);
-        navigationBarFragment    = initFragment(R.id.call_log_navigation_bar_container, new NavigationBarFragment());
+        callLogTV = findViewById(R.id.CallLogsTextView);
+        callLogTV.setText("dsfdfsdfdsfdsfdsfdsffdfsd");
+        callLogsContainer = findViewById(R.id.call_log_call_log_fragment_container);
+        callLogsFragment    = initFragment(R.id.call_log_navigation_bar_container, new CallLogsFragment());
+        navigationBarContainer = findViewById(R.id.call_log_navigation_bar_container);
+        navigationBarFragment = initFragment(R.id.call_log_navigation_bar_container, new NavigationBarFragment());
     }
-    @Override
-    public void onFragmentInteraction(Uri uri){
+
+
+
+        @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
