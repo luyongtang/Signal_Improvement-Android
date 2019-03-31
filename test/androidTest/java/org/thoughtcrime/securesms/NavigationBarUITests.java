0cb4429a97a1b45fda18f95ee09ad2a2f25ad197
Marc-Henry Noon <marc.noon@gmail.com>
2019-03-31 19:54:49 -0400

    Changed NavBar UI tests
    The tests covers the "chats" and "Settings" tabs. Coverage for "Call Logs" will be added when that feature is ready

--- test/androidTest/java/org/thoughtcrime/securesms/NavigationBarUITests.java
+++ test/androidTest/java/org/thoughtcrime/securesms/NavigationBarUITests.java
@@ -1,7 +1,9 @@
 package org.thoughtcrime.securesms;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.SystemClock;
+import android.preference.PreferenceActivity;
 import android.support.design.internal.NavigationMenuItemView;
 import android.widget.TimePicker;
 
@@ -9,14 +11,19 @@ import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.thoughtcrime.securesms.util.TextSecurePreferences;
 
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Iterator;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import androidx.test.runner.lifecycle.Stage;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
@@ -33,6 +40,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.runner.lifecycle.Stage.RESUMED;
 import static org.mockito.Mockito.mock;
 
 @RunWith(AndroidJUnit4.class)
@@ -42,23 +51,11 @@ public class NavigationBarUITests {
     //In order to run those tests, an existing conversation with +15146556003 is required
     public ActivityTestRule<ConversationListActivity> myActivity = new ActivityTestRule<>(ConversationListActivity.class);
 
-
     @Test
-    public void selectCallogs(){
-        onView(withId(R.id.navigation_bar_call_logs)).perform(click());
-        onView(withClassName(Matchers.equalTo(CallLogsActivity.class.getName()))).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void selectChat(){
-        onView(withId(R.id.navigation_bar_chats)).perform(click());
-        onView(withClassName(Matchers.equalTo(ConversationListActivity.class.getName()))).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void selectSettings(){
+    public void selectSettingsThenBackToConversation(){
         onView(withId(R.id.navigation_bar_settings)).perform(click());
-        onView(withClassName(Matchers.equalTo(ApplicationPreferencesActivity.class.getName()))).check(matches(isDisplayed()));
-
+        onView(withText("Analytics Report")).check(matches(isDisplayed()));
+        onView(withId(R.id.navigation_bar_chats)).perform(click());
+        onView(withContentDescription("New conversation")).check(matches(isDisplayed()));
     }
 }
